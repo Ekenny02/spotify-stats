f58ba46643324d0997955ad421fade01418db9cf
ekenny02 <ekenny456@gmail.com>
2024-06-02 14:53:08 -0700

    Fixed Merge Issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Text, View} from "react-native";
 import Entypo from "@expo/vector-icons/Entypo";
 import LoginButton from "./components/LoginButton";
+import React from "react";
 
 export default function App() {
 
