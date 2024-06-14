4bac8fdcc3f326c87fe9319fbdf8dac606f4f38a
ekenny02 <ekenny456@gmail.com>
2024-06-13 19:16:00 -0700

    Added Top 5 Songs to Top Artists Display

--- src/components/ArtistDisplay.tsx
+++ src/components/ArtistDisplay.tsx
@@ -1,29 +1,36 @@
 import {useEffect, useState} from "react";
 import {View, Text, Image} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function ArtistDisplay(props: any) {
   const [artist, setArtist] = useState<any>({});
 
   useEffect(() => {
-    props.artist["image_url"] = props.artist["images"][0]["url"];
     setArtist(props.artist);
   }, []);
 
   return (
     <View
       id={"item-" + props.position}
-      className="items-center m-2">
-      <Image
-        className="rounded-full"
-        id="picture"
-        source={{uri: artist["image_url"], height: 150, width: 150}}
-      />
-      <Text
-        id="name"
-        className="text-xl font-extrabold">
-        {artist["name"]}
-      </Text>
+      className="mx-3 my-5 flex-row">
+      <View className="items-center w-[40%]">
+      <Text className="absolute left-0 text-lg font-bold">{props.position}.</Text>
+        <Image
+          className="rounded-full"
+          id="picture"
+          source={artist["images"] && artist["images"][0] && {uri: artist["images"][0]["url"], height: 100, width: 100}}
+        />
+        <Text
+          id="name"
+          className="text-xl font-extrabold">
+          {artist["name"]}
+        </Text>
+      </View>
+      <View className="w-[60%]">
+      <Text className="text-lg font-bold">Top Songs:</Text>
+        {artist["top_songs"] && artist["top_songs"].map((song: any, position: number) => 
+        <Text numberOfLines={1} key={position}>{position + 1}. {song['song_name']}</Text>
+        )}
+      </View>
     </View>
   );
 }
--- src/screens/Tabs/Statistics.tsx
+++ src/screens/Tabs/Statistics.tsx
@@ -2,45 +2,73 @@ import {useEffect, useState} from "react";
 import {Text, SafeAreaView, View, FlatList} from "react-native";
 import GetData from "../../api/GetData";
 import ArtistDisplay from "../../components/ArtistDisplay";
-import { FlashList } from "@shopify/flash-list";
+import {FlashList} from "@shopify/flash-list";
 
 export default function Statistics() {
   const [topArtists, setTopArtists] = useState<Array<any>>([]);
 
   useEffect(() => {
-    GetData({
-      extension: "v1/me/top/artists",
-      method: "GET",
-      url_search_params: new URLSearchParams({
-        limit: "20",
-      }),
-    }).then((data) => {
-      console.log(Object.keys(data["items"][0]));
-      setTopArtists(data["items"]);
-    });
+    async function processData() {
+      const artistData = await GetData({
+        extension: "v1/me/top/artists",
+        method: "GET",
+        url_search_params: new URLSearchParams({
+          limit: "20",
+        }),
+      });
+
+      for (const artist of artistData["items"]) {
+        const artistTracks = await GetData({
+          extension: `v1/artists/${artist["id"]}/top-tracks`,
+          method: "GET",
+        });
+
+        for (let i = 0; i < 5; i++) {
+          if (!artist["top_songs"]) {
+            artist["top_songs"] = [];
+          }
+  
+          artist["top_songs"].push({
+            song_name: artistTracks["tracks"][i]["name"],
+            album_image_url: artistTracks["tracks"][i]["album"]["images"][0]["url"],
+          });
+        }
+      }
+
+      setTopArtists(artistData["items"]);
+    }
+
+    processData();
   }, []);
 
   return (
     <SafeAreaView
       id="statistics-tab"
       className="flex-1 justify-center items-center">
-      <View id="header" className="h-[5%]">
-        <Text id="title" className="text-2xl font-bold">Top Artists</Text>
+      <View
+        id="header"
+        className="h-[5%]">
+        <Text
+          id="title"
+          className="text-2xl font-bold">
+          Top Artists
+        </Text>
       </View>
       <View className="w-full h-[95%]">
-      {topArtists && (
-        <FlashList
-          id="items"
-          showsVerticalScrollIndicator={false}
-          data={topArtists}
-          estimatedItemSize={20}
-          renderItem={({item, index}) => (
-            <ArtistDisplay
-              key={index}
-              artist={item}
-              position={index + 1}></ArtistDisplay>
-          )}
-        />)}
+        {topArtists && (
+          <FlashList
+            id="items"
+            showsVerticalScrollIndicator={false}
+            data={topArtists}
+            estimatedItemSize={20}
+            renderItem={({item, index}) => (
+              <ArtistDisplay
+                key={index}
+                artist={item}
+                position={index + 1}></ArtistDisplay>
+            )}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
